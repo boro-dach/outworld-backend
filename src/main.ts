7df223fc6e2fad3ecba80e42621cdc040a6df162
boro-dach <140444699+boro-dach@users.noreply.github.com>
2024-12-31 17:47:17 +0100

    origin update for vercel

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://outworld-web.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true
